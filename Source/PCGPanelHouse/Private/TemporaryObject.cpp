b1d9422fc2be028f0e2493303eae92ce9dc690b6
clutch-or-kick <serhii.poklonskyi@gmail.com>
2026-04-17 17:07:57 +0200

    panel building grammar
    initial commit and some parsing logic implemented

---
+++ Source/PCGPanelHouse/Private/TemporaryObject.cpp
@@ -0,0 +1,195 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PCGPanelHouse/Public/TemporaryObject.h"
+#include "Algo/Count.h"
+
+/**
+ * check's the rules validity (the rule should be non-empty and must not consist of a single optional panel group)
+ * @param Rule 
+ * @param OutError 
+ * @return 
+ */
+bool ValidatePBRule(const FPBRule& Rule, FParsingError& OutError)
+{
+	if (Rule.Items.IsEmpty())
+	{
+		OutError.ErrorAtPosition("Empty rules are not allowed", Rule.Position);
+		return false;
+	}
+
+	if (Algo::CountIf(Rule.Items,
+	                  [](const FPBRuleItem& Item)
+	                  {
+		                  return Item.IsType<FPanelGroup>();
+	                  }) > 1)
+	{
+		OutError.ErrorAtPosition("A rule cannot contain more than one group", Rule.Position);
+		return false;
+	}
+
+	if (Rule.Items.Num() == 1 && Rule.Items[0].IsType<FPanelGroup>() && !Rule.Items[0].Get<FPanelGroup>().
+		AtLeastOneOccurrence)
+	{
+		OutError.ErrorAtPosition("Rules containing a single optional group are not allowed", Rule.Position);
+		return false;
+	}
+
+	return true;
+}
+
+bool ParsePBGroup(const FString& Grammar, int& Iterator, FPanelGroup& OutPanelGroup, FParsingError& OutError)
+{
+	FString CurrentSubstring;
+
+	if (Iterator >= Grammar.Len())
+	{
+		OutError.ErrorAtPosition("Expected panel group, found EOL", Iterator);
+		return false;
+	}
+
+	for (int& i = Iterator; i < Grammar.Len(); i++)
+	{
+		TCHAR Character = Grammar[i];
+
+		if (FChar::IsDigit(Character))
+		{
+			CurrentSubstring.AppendChar(Character);
+		}
+		else if (Character == TEXT('-'))
+		{
+			if (CurrentSubstring.IsEmpty())
+			{
+				OutError.ErrorAtPosition("Expected panel index, found '-'", Iterator);
+				return false;
+			}
+
+			OutPanelGroup.PanelIndices.Add(FCString::Atoi(*CurrentSubstring));
+			CurrentSubstring.Empty();
+		}
+		else if (Character == TEXT('{'))
+		{
+			OutError.ErrorAtPosition("Nexted panel groups are not allowed", Iterator);
+			return false;
+		}
+		else if (Character == TEXT('}'))
+		{
+			if (!CurrentSubstring.IsEmpty())
+			{
+				OutPanelGroup.PanelIndices.Add(FCString::Atoi(*CurrentSubstring));
+				CurrentSubstring.Empty();
+			}
+
+			Iterator++;
+			break;
+		}
+		else
+		{
+			OutError.ErrorAtPosition("Unexpected character", Iterator);
+			return false;
+		}
+	}
+
+	if (OutPanelGroup.PanelIndices.IsEmpty())
+	{
+		OutError.ErrorAtPosition("Empty groups are not allowed", OutPanelGroup.Position);
+		return false;
+	}
+
+	if (Iterator >= Grammar.Len())
+	{
+		OutError.ErrorAtPosition("Expected group reoccurrence specifier, found EOL", Iterator);
+		return false;
+	}
+
+	// group reoccurrence specifier ('*' or '+')
+	TCHAR ReoccurrenceSpecifier = Grammar[Iterator++];
+	if (ReoccurrenceSpecifier == TEXT('+'))
+	{
+		OutPanelGroup.AtLeastOneOccurrence = true;
+	}
+	else if (ReoccurrenceSpecifier != TEXT('*'))
+	{
+		OutError.ErrorAtPosition("Expected a group reoccurrence specifier", Iterator);
+		return false;
+	}
+
+	return true;
+}
+
+bool ParsePBGrammar(const FString& Grammar, FPBRuleSet& OutRuleSet, FString& OutErrorMessage, FParsingError& OutError)
+{
+	if (Grammar.TrimStartAndEnd().IsEmpty())
+	{
+		OutError.ErrorAtPosition("Cannot parse an empty grammar", -1);
+		return false;
+	}
+
+	FPBRuleSet PBRuleSet;
+	FPBRule CurrentPBRule;
+	FString CurrentSubstring;
+
+	for (int i = 0; i < Grammar.Len(); i++)
+	{
+		if (const TCHAR Character = Grammar[i]; FChar::IsDigit(Character))
+		{
+			CurrentSubstring.AppendChar(Character);
+		}
+		else if (Character == TEXT('-'))
+		{
+			if (CurrentSubstring.IsEmpty())
+			{
+				OutError.ErrorAtPosition("Expected panel index, found '-'", i);
+				return false;
+			}
+
+			CurrentPBRule.Items.Add(FPBRuleItem<int>{FCString::Atoi(*CurrentSubstring)});
+			CurrentSubstring.Empty();
+		}
+		else if (Character == TEXT('{'))
+		{
+			FPanelGroup PanelGroup;
+			PanelGroup.Position = i;
+			
+			FParsingError GroupParsingError;
+
+			auto Success = ParsePBGroup(Grammar, ++i, PanelGroup, GroupParsingError);
+			if (!Success)
+			{
+				OutError.ErrorAtPosition(GroupParsingError.ErrorMessage, GroupParsingError.Position);
+				return false;
+			}
+
+			CurrentPBRule.Items.Add(FPBRuleItem<FPanelGroup>{PanelGroup});
+
+			// EOL after reading the group - validate the current rule before exiting
+			if (i >= Grammar.Len())
+			{
+				if (!ValidatePBRule(CurrentPBRule, OutError))
+				{
+					return false;
+				}
+
+				PBRuleSet.Rules.Add(CurrentPBRule);
+				break;
+			}
+		}
+		else if (Character == TEXT('|'))
+		{
+			if (!ValidatePBRule(CurrentPBRule, OutError))
+			{
+				return false;
+			}
+
+			PBRuleSet.Rules.Add(CurrentPBRule);
+			CurrentPBRule = FPBRule();
+		}
+		else
+		{
+			OutError.ErrorAtPosition("Unexpected character", i);
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ Source/PCGPanelHouse/Public/TemporaryObject.h
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "TemporaryObject.generated.h"
+
+struct FParsedPBExpression
+{
+	int Position; // the position in the grammar that this expression was parsed at
+};
+
+/**
+ * describes a reoccurring group of panels
+ * 
+ * in the panel building rule grammar, a group is enclosed in curly braces:
+ * 
+ * 1-2-{1-2-3}*-2-1
+ *
+ * - the number of occurrences can be set to 0 or more (*) or 1 or more (+)
+ * - nested groups are not allowed
+ * - a single rule can contain only one panel group
+ */
+struct FPanelGroup : FParsedPBExpression
+{
+	TArray<int> PanelIndices;
+	bool AtLeastOneOccurrence {false};
+};
+
+using FPBRuleItem = TVariant<int, FPanelGroup>;
+
+/**
+ * describes a rule for placement of panels
+ *
+ * The rule is a grammar of form:
+ *
+ * 1-2-{1-2-3}*-2-1
+ */
+struct FPBRule : FParsedPBExpression
+{
+	TArray<FPBRuleItem> Items;
+};
+
+enum EPanelBuildingSide
+{
+	Front,
+	Right,
+	Back,
+	Left
+};
+
+/**
+ * describes a set of rules for placement of panels in a panel building
+ *
+ * the pipe `|` denotes the corner of the building
+ *
+ * the rules are defined in the following order:
+ * - front side
+ * - right side
+ * - back side
+ * - left side
+ *
+ * front/right/back/left are defined from actor's forward/right vectors
+ */
+struct FPBRuleSet
+{
+	TArray<FPBRule> Rules; // rules defined by the user
+	
+	const FPBRule GetPBRule(EPanelBuildingSide Side) const;
+};
+
+/**
+ * an error that occurred while parsing the panel building grammar
+ */
+struct FParsingError
+{
+	FString ErrorMessage;
+	int Position {-1};
+
+	void ErrorAtPosition(const FString& ErrorMessage, const int Position)
+	{
+		this->ErrorMessage = ErrorMessage;
+		this->Position = Position;
+	}
+};
+
+/**
+ * parse a grammar string (like 1-2-{2-3}+-2-1)
+ * 
+ * @param Grammar
+ * @param OutRuleSet
+ * @param OutErrorMessage information on parsing errors
+ * @return 
+ */
+bool ParsePBGrammar(const FString& Grammar, FPBRuleSet& OutRuleSet, FString& OutErrorMessage);
+
+/**
+ * 
+ */
+UCLASS()
+class PCGPANELHOUSE_API UTemporaryObject : public UObject
+{
+	GENERATED_BODY()
+};
